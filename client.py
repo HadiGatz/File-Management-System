1dc121b7b813a7d7fa0a510251d73e7adf8d6cf0
Hadi Gatz <albertsviridovski@gmail.com>
2024-08-04 11:45:35 +0300

    [ADD] turned the logic in the server into a function

--- client.py
+++ client.py
@@ -4,24 +4,48 @@ import os
 HOST = '127.0.0.1'
 PORT = 8820
 
-# getting the file and its name
-file_directory = input("Enter the address of your file: ").strip().strip('"')
-file_basename = os.path.basename(file_directory)
+# SAVEFILE - sends a file to the server
+# GETFILE - gets a lists of files from the server, then gets the file 
 
-# reading the content of the file
-with open(file_directory, 'rb') as f:
-    file_data = f.read()
+
 
 # connecting to the server
 client_socket = socket.socket()
 client_socket.connect((HOST, PORT))
 
-# sending the file name
-client_socket.send(file_basename.encode())
+operation = input("Enter your operation: ")
 
-# sending the file data in chunks
-client_socket.sendall(file_data)
 
-print("Information sent to the server")
+
+def SAVEFILE():
+    # getting the file and its name
+    file_directory = input("Enter the address of your file: ").strip().strip('"')
+    file_basename = os.path.basename(file_directory)
+
+    # reading the content of the file
+    with open(file_directory, 'rb') as f:
+       file_data = f.read()
+
+    # sending the length of the file name
+    file_name_length = len(file_basename.encode())
+    client_socket.send(file_name_length.to_bytes(2, byteorder='big'))
+
+    # sending the file name
+    client_socket.send(file_basename.encode(encoding='utf-8'))
+
+    # sending the file data in chunks
+    client_socket.sendall(file_data)
+
+    print("Information sent to the server")
+
+def GETFILE():
+    pass
+
+if operation == "SAVEFILE":
+    SAVEFILE()
+elif operation == "GETFILE":
+    GETFILE()
+else:
+    print("Error: Enter a valid operation")
 
 client_socket.close()
--- server.py
+++ server.py
@@ -20,26 +20,33 @@ if not os.path.exists(file_directory):
 server = socket.socket()
 server.bind((HOST, PORT))
 
-def handle_client(client_socket, client_address):
-    file_name = client_socket.recv(1024).decode()
-    file_content = client_socket.recv(1024)
-    file_path = os.path.join(file_directory, file_name)
+def handle_client_sending_file(client_socket, client_address):
+    try:
+        file_name_length = int.from_bytes(client_socket.recv(2), byteorder='big')
+        print(file_name_length)
+        file_name = client_socket.recv(file_name_length).decode(encoding='utf-8')
+        print(file_name)
+        file_path = os.path.join(file_directory, file_name)
+        
+        with open(file_path, 'wb') as file:
+            while True:
+                file_content = client_socket.recv(1024)
+                if not file_content:
+                    break
+                file.write(file_content)
 
-    with open(file_path, 'wb') as file:
-        while True:
-            file_content = client_socket.recv(1024)
-            if not file_content:
-                 break
-            file.write(file_content)
-
-    print(f"[RECEIVED] {file_name} from {client_address}")
+        print(f"[RECEIVED] {file_name} from {client_address}")
+    except Exception as e:
+        print(f"[ERROR] An error occurred while handling the client: {e}")
+    finally:
+        client_socket.close()
 
 def start_server():
     server.listen()
     print("[RUNNING]")
     client_socket, client_address = server.accept()
     print(f"[HANDLING] {client_address}")
-    handle_client(client_socket, client_address)
+    handle_client_sending_file(client_socket, client_address)
 
 running = True
 while running:
